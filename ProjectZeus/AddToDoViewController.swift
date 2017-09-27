066e3e75b14a7cb5c86a6e6ed417600c86a3ffdb
Unknown <itomclements@googlemail.com>
2017-09-27 22:48:11 +0100

    Initial commit

---
+++ ProjectZeus/AddToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoViewController.swift
+//  ProjectZeus
+//
+//  Created by Thomas Clements on 24/09/2017.
+//  Copyright © 2017 Thomas Clements. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProjectZeus/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ProjectZeus
+//
+//  Created by Thomas Clements on 24/09/2017.
+//  Copyright © 2017 Thomas Clements. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
